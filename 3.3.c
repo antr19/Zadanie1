e2707889415ca3cc7404e97d350f156ad69bdd29
antr19 <32762114+antr19@users.noreply.github.com>
2017-12-15 13:07:35 +0300

    Update 3.3.c

--- 3.3.c
+++ 3.3.c
@@ -1,30 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int F(n)
+{
+    if(n==1)
+    {
+        return 1;
+    }
+    else if (n==2)
+    {
+        return 1;
+    }
+    else
+        return (F(n-1)+F(n-2));
+}
+
 int main()
 {
     int n;
     scanf("%d",&n);
     if (n<=0)
-    {
         return -1;
-    }
-    int F(n)
-    {
-        if(n==1)
-        {
-            return 1;
-        }
-        else if (n==2)
-        {
-            return 1;
-
-        }
-        else
-        {
-            return (F(n-1)+F(n-2));
-        }
-    }
     printf("%d",F(n));
     return 0;
 }
