54109b8eb9b63732ec0aa91d90e0091e71b08646
antr19 <32762114+antr19@users.noreply.github.com>
2017-12-20 00:47:14 +0300

    Update 4.1.c

--- 4.1.c
+++ 4.1.c
@@ -1,54 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+int comp(const void* a, const void* b)
 {
-    int n,i,mt,max,min,m,M,k;
-    scanf("%d",&n);
-    int* S = malloc(n*sizeof(int));
-    int* Mas = malloc(n*sizeof(int));
-    for (i=1;i<=n;i++)
+    int A = *(int*) a;
+    int B = *(int*) b;
+    return A - B;
+}
+
+int main(void)
+{
+    int n, k, i, mt;
+    scanf("%d", &n);
+    int* S =(int*) malloc(n * sizeof(int));
+    int* Mas =(int*) malloc(n * sizeof(int));
+
+
+    for (i = 0; i < n; i++)
     {
-        scanf("%d %d",&Mas[i],&S[i]);
+        scanf("%d %d", &Mas[i], &S[i]);
     }
-    max = 0;
+
+    qsort(Mas, n, sizeof(int), comp); // Эту магию мне рассказал ученик Третьяковаа
+    qsort(S, n, sizeof(int), comp);
+
     mt = 0;
-    k=0;
-    M=0;
-    for (i=1;i<=n;i++)
-    {
-        if (S[i]>max)
-        {
-            max=S[i];
-            M=i;
-        }
+    k = 0;
 
-    }
-    printf("%d    %d \n",max,M+1);
-    while (mt<=max)
+    while (mt <= Mas[n - 1])
     {
+        if (k == (n + 1))
+            break;
+
+        mt += Mas[k];
+
+        printf("#%d Massa%d Sila%d\n", k+1, Mas[k], S[k]);
         k++;
-        min=S[1];
-        m=1;
-        for(i=1;i<n;i++)
-        {
-            if (min>S[i])
-            {
-                min=S[i];
-                m=i;
-            }
-
-        }
-        if (min!=max)
-        {
-            printf("%d %d \n",k,m+1);
-            mt=mt+Mas[m];
-            S[m]=S[M];
-        }
     }
-    printf("%d %d \n",k,M+1);
-    printf("%d \n",k);
-free(S);
-free(M);
-return 0;
+    return 0;
 }
